f6f8128f0cad3bc988810f3b3ccb9937482a1b63
Sharad Pandey <sharad868788@gmail.com>
2024-08-03 11:22:50 +0530

    Serve is reset for now

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
-//const mongooseURL = 'mongodb://localhost:27017/hotel'
-//const mongooseURL = 'mongodb+srv://sharad_Pandey:sharad1234@cluster0.exxwvkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+//const mongooseURL = ''
+//const mongooseURL = '
 const mongooseURL = process.env.db_URL;
 mongoose.connect(mongooseURL, {
     useNewUrlParser: true,
